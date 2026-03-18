dd649c0864f49e76600a6beec3273d27e13d74ab
UlisesLlopiUTN <llopiulisesalan10@ca.frre.utn.edu.ar>
2026-03-17 21:54:29 -0300

    educacion

--- src/app/components/educacion/educacion.component.ts
+++ src/app/components/educacion/educacion.component.ts
@@ -6,39 +6,71 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-educacion',
   templateUrl: './educacion.component.html',
-  styleUrls: ['./educacion.component.css']
+  styleUrls: ['./educacion.component.css'],
 })
 export class EducacionComponent implements OnInit {
   educacion: Educacion[] = [];
-  constructor(private educacionS:EducacionService, private tokenService: TokenService) { }
+  constructor(
+    private educacionS: EducacionService,
+    private tokenService: TokenService,
+  ) {}
   isLogged = false;
 
   ngOnInit(): void {
     this.cargarEducacion();
-    if(this.tokenService.getToken()){
-      this.isLogged=true;
-    }else {
-      this.isLogged=false;
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
     }
   }
 
-  cargarEducacion(): void{
-    this.educacionS.lista().subscribe(
-      data=>{
-        this.educacion = data;
+  cargarEducacion(): void {
+    // this.educacionS.lista().subscribe(
+    //   data=>{
+    //     this.educacion = data;
+    //   }
+    // )
+
+    const educacionS: Educacion[] = [
+      {
+        id: 1,
+        nombreE: 'Ingenieria en Sistemas de Información',
+        descripcionE:
+          'Estudiante avanzado en la Universidad Tecnológica Nacional en Ingeniria en Sistemas de Información. (4to año)',
+      },
+      {
+        id:2,
+        nombreE:'Técnico en Informatica profesional y personal',
+        descripcionE:'Recibido de escuela Técnica Carmen Molina de Llano y Anexo San Vicente de Paul'
+      },
+      {
+        id: 3,
+        nombreE: 'Curso de Desarrollo',
+        descripcionE:'Desarrollo Web Full Stack Junior en Java (Argentina Programa - #YoProgramo) '
+      },
+      {
+        id:4,
+        nombreE:'Curso sobre IA',
+        descripcionE:'Introducción | Formación en Datos e Inteligencia Artificial (Argentina Programa 4.0)'
       }
-    )
+
+    ];
+
+    // Asignamos directamente los datos estáticos a la variable que usa el HTML
+    this.educacion = educacionS;
   }
 
-  delete(id?: number){
-    if(id != undefined){
+  delete(id?: number) {
+    if (id != undefined) {
       this.educacionS.delete(id).subscribe(
-        data=> {
+        (data) => {
           this.cargarEducacion();
-        }, err => {
-          alert("no se pudo eliminar educacion");
-        }
-      )
+        },
+        (err) => {
+          alert('no se pudo eliminar educacion');
+        },
+      );
     }
   }
 }
--- src/app/components/hys/hys.component.ts
+++ src/app/components/hys/hys.component.ts
@@ -6,38 +6,118 @@ import { SkillService } from 'src/app/service/skill.service';
 @Component({
   selector: 'app-hys',
   templateUrl: './hys.component.html',
-  styleUrls: ['./hys.component.css']
+  styleUrls: ['./hys.component.css'],
 })
 export class HysComponent implements OnInit {
   skill: Skill[] = [];
 
-  constructor(private skillS: SkillService, private tokenService: TokenService) { }
+  constructor(
+    private skillS: SkillService,
+    private tokenService: TokenService,
+  ) {}
   isLogged = false;
   ngOnInit(): void {
     this.cargarSkills();
-    if(this.tokenService.getToken()){
+    if (this.tokenService.getToken()) {
       this.isLogged = true;
     } else {
       this.isLogged = false;
     }
   }
 
-  cargarSkills():void{
-    this.skillS.lista().subscribe(
-      data=> {
-        this.skill = data;
-      }
-    )
+  cargarSkills(): void {
+    // this.skillS.lista().subscribe(
+    //   data=> {
+    //     this.skill = data;
+    //   }
+    // )
+
+    const skillS: Skill[] = [
+      {
+        id: 1,
+        nombre: 'Vue.js',
+        porcentaje: 80,
+      },
+      {
+        id: 2,
+        nombre: 'React.js',
+        porcentaje: 60,
+      },
+      {
+        id: 3,
+        nombre: 'Next.js',
+        porcentaje: 60,
+      },
+      {
+        id: 13,
+        nombre: 'Java',
+        porcentaje: 50,
+      },
+      {
+        id: 14,
+        nombre: 'Node.js',
+        porcentaje: 70,
+      },
+      {
+        id: 4,
+        nombre: 'TypeScript',
+        porcentaje: 60,
+      },
+      {
+        id: 5,
+        nombre: 'JavaScript',
+        porcentaje: 80,
+      },
+      {
+        id: 6,
+        nombre: 'Golang',
+        porcentaje: 30,
+      },
+      {
+        id: 7,
+        nombre: 'Laravel',
+        porcentaje: 40,
+      },
+      {
+        id: 8,
+        nombre: 'Tailwind CSS',
+        porcentaje: 80,
+      },
+      {
+        id: 9,
+        nombre: 'Bootstrap',
+        porcentaje: 70,
+      },
+      {
+        id: 10,
+        nombre: 'PrimeVue',
+        porcentaje: 80,
+      },
+      {
+        id: 11,
+        nombre: 'Ingles',
+        porcentaje: 50,
+      },
+      {
+        id: 12,
+        nombre: 'GitHub',
+        porcentaje: 70,
+      },
+    ];
+
+    // Asignamos directamente los datos estáticos a la variable que usa el HTML
+    this.skill = skillS;
   }
-  delete(id: number){
-    if(id != undefined){
+  delete(id: number) {
+    if (id != undefined) {
       this.skillS.delete(id).subscribe(
-        data=>{
+        (data) => {
           this.cargarSkills();
-        }, err =>{
-          alert("no se puede borrar la skill");
-        }
-      )
+        },
+        (err) => {
+          alert('no se puede borrar la skill');
+        },
+      );
     }
   }
 }
